4614c070b13459441a99b2bcc2b87566ad88f603
HiricaAlexandru <72924378+HiricaAlexandru@users.noreply.github.com>
2023-05-30 00:38:47 +0300

    Stabilized the delay and message transmission

--- nrz/reciever/reciever.ino
+++ nrz/reciever/reciever.ino
@@ -1,22 +1,25 @@
-#define D2 2
-#define D7 7
-#define D4 4
-
-#define character_duration 500
-#define clock_duration character_duration / 2
+#define D2 2 //message pin
+#define D7 7 // clock pin
+#define D4 4 // debug clock pin
+#define D5 5 // sync pin
 
 bool isChangedClock = false;
+bool isChangedSync = false;
 char result = 0;
 int number = 0;
 int parityGot, parityCalculated = false;
 int currentIndex = 0;
 char buffer[200];
 
+inline bool getBitValue(char* buffer) __attribute__((always_inline));
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(D2, INPUT);
   pinMode(D7, INPUT);
   pinMode(D4, OUTPUT);
+  pinMode(D5, INPUT);
+
   Serial.begin(9600);
 
   int clockValue = digitalRead(D7);
@@ -29,11 +32,31 @@ void setup() {
 bool getBitValue(char* buffer)
 {
   int clockValue = digitalRead(D7);
+  bool syncValue = digitalRead(D5);
+
+  //isChangedSync = isChangedSync ^ syncValue;
+
+  //if(isChangedSync && syncValue == true){
+  //  number = 0;
+  //  parityCalculated = 0;
+  //  result = 0;
+  //}
+
+  if(isChangedSync != syncValue){
+    isChangedSync = syncValue;
+
+    if(syncValue == true){
+      number = 0;
+      parityCalculated = 0;
+      result = 0;
+    }
+  }
 
   if(isChangedClock != clockValue){
     //if the clock is up then we will read the value of the incoming bit
 
     isChangedClock = clockValue;
+
     if(clockValue == 1){
       number+=1;
       int messageValue;
@@ -57,13 +80,15 @@ bool getBitValue(char* buffer)
     parityGot = result & 128; //getting the got parity
 
     result -= parityGot; //eliminating the parity from the got message
-
+    
     if((parityCalculated == 1 && parityGot>0) || (parityGot == parityCalculated)){
+      //parityGot could be 0 and 128
       buffer[currentIndex++] = result;
     }
     else{
-      buffer[currentIndex++] = result;
+      buffer[currentIndex++] = '?';
     }
+
     result = 0;
     number = 0;
     parityCalculated = false;
@@ -81,7 +106,6 @@ void loop() {
   // put your main code here, to run repeatedly:
   bool finished = getBitValue(buffer);
   if(finished == true){
-    Serial.println(buffer[0]);
     Serial.println(buffer);
     currentIndex = 0;
   }
--- nrz/sender/sender.ino
+++ nrz/sender/sender.ino
@@ -1,24 +1,27 @@
 #define D2 2
 #define D3 3
-#define character_duration 50//320
+#define D4 4
+#define character_duration 28//32 are pierderi de informatii
 #define clock_duration character_duration / 2
 
 inline void break_char_into_byte(const char character) __attribute__((always_inline));
 inline void break_string_into_characters(const char* string, uint32_t length) __attribute__((always_inline));
 
-char *stringAnalyze = "Hello world!";
-
 void setup() {
   // put your setup code here, to run once:
-  pinMode(D2, OUTPUT);
-  pinMode(D3, OUTPUT);
+  pinMode(D2, OUTPUT); //message pin
+  pinMode(D3, OUTPUT); //clock pin
+  pinMode(D4, OUTPUT); //sync pin
   Serial.begin(9600);
   
-  delay(10000);
 }
 
 void sendString(char* message)
 {
+  // the clock will stay on for clock_duration microseconds
+  // the character will be sent over character_duration microseconds 
+  // the sync will stay on for clock_duration microseconds
+
   int currentBit, length, i, j;
   char character;
   bool charParity; //if the number of ones is odd then the parity will be true
@@ -26,8 +29,11 @@ void sendString(char* message)
   length = strlen(message);
 
   for(i = 0;i<=length;i++){
+
+    digitalWrite(D4, HIGH);
     character = message[i];
     charParity = false;
+
     for(j = 0;j<8;j++){
       digitalWrite(D2, HIGH); //clock
 
@@ -36,6 +42,7 @@ void sendString(char* message)
       if(currentBit == 1){
         charParity = charParity ^ currentBit;
       }
+
       if(j == 7){
         digitalWrite(D3, charParity);
         //Serial.print(charParity);
@@ -44,37 +51,25 @@ void sendString(char* message)
         //Serial.print(currentBit);
       }
 
-      //delayMicroseconds(clock_duration);
-      delay(clock_duration);
+      //delay to switch the clock and refresh the message
+      delayMicroseconds(clock_duration);
+      //delay(clock_duration);
       digitalWrite(D2, LOW);
-      delay(clock_duration);
-      //delayMicroseconds(clock_duration);
+      //delay(clock_duration);
+      delayMicroseconds(clock_duration);
+      digitalWrite(D4, LOW); //disabling the sync pin because it resetted the string in the receiver
     }
   }
+  //after each transmission, 100 microseconds pause
+  delayMicroseconds(100);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  sendString(stringAnalyze);
-  delay(4000);
+  sendString("Hello tata, bine te-am gasit sunt happy azi!");
+  //delay(1);
+  sendString("Hello mama!");
+  //delay(4000);
   sendString("Bubu honey");
-}
-
-void break_char_into_byte(const char character) 
-{
-  uint8_t currentChar, i;
-
-  for(i = 0; i<8;i++){
-    currentChar = (character >> i) & 1;
-    Serial.print(currentChar);
-  }
-}
-
-void break_string_into_characters(const char* string, uint32_t length)
-{
-  uint32_t i;
-  for(i = 0; i < length; i++)
-  {
-    break_char_into_byte(string[i]);
-  }
+  //delay(1);  
 }
